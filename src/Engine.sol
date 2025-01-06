ac6e333c0ccaa0b5d7db04a2189163dece56c472
Ryzen XP <iron4540@gmail.com>
2025-01-06 14:25:25 +0530

    add interface

---
+++ src/Engine.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract XP_Engine {
+  
+  function deposit_colletral_mintXP() external {}
+
+  function redeem_colletral_for_XP()external {}
+
+  function burn_XP()external{}
+
+  function liquidate()external{}
+}
---
+++ test/stablecoin.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier:UNLICENSED
+pragma solidity ^0.8.13 ;
+
+import {test , console} from "forge-std/";
+import {stablecoin} from "../src/stablecoin.sol";
+
+/**
+ * @title: test for stable coin  it test all edge cases that
+ * 
+ */
+
+contract Test is {stablecoin}{
+  
+
+}
+
+
+
+                                
