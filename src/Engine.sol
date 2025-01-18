e08b5d00803aa70f842184ea134279ececeafc02
Ryzen XP <iron4540@gmail.com>
2025-01-18 11:33:53 +0530

    add constructor prams

--- src/Engine.sol
+++ src/Engine.sol
@@ -6,25 +6,27 @@ import {Stablecoin} from "./stablecoin.sol";
 import "../lib/chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 
-contract XP_Engine  is Stablecoin {
-  AggregatorV3Interface internal eth_usd_PriceFeed;
+contract XP_Engine  {
+  AggregatorV3Interface internal eth_usd_PriceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306 );
 
-  /**
- * Network: Sepolia
- * Data Feed: ETH/USD
- * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
- * 
- */
 
-constructor() {
-  eth_usd_PriceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306 );
+
+constructor(address[] memory CollateralAddress , address[] memory price_feed_address , address xp_address ) {
+  if( CollateralAddress.length != price_feed_address.length ){
+    revert XP_not_equal_ratio();
+  }
+  else{
+    for(uint i=0 ; i< CollateralAddress.length ; i++){
+     xp_allowed_token[CollateralAddress[i]]= price_feed_address[i];
+
+    }
+    xp_token = xp_address;
+  }
+  
   
 }
 
-/**
- * get_price_feed   return ETH->USD price 
- *   
- */
+
 
   function get_price_feed()public view returns(int256) {
     (, int256 eth_usd, , , ) = eth_usd_PriceFeed.latestRoundData();
@@ -33,34 +35,60 @@ constructor() {
   }
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////
- ////////////////////                  ERRORS                              ////////////////////////////// 
+ ////////////////////                  state variable                           
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ Stablecoin private immutable stablecoin ;
+ address private immutable xp_token ;
+
+
+
+  // Colletral   deposited by user  mapping 
+  mapping(address => mapping (address colletralAddress => uint amount)) private xp_colletralDeposite ;
+
+  // Collatreal address which allowed 
+  mapping(address colletralAddress =>address price_feed_address) private xp_allowed_token ;
+
+  mapping (address => uint) private x_XPMinted ;
+ 
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+ ////////////////////                  ERRORS                            
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
         error  XP_amount_zero();
+        error XP_not_equal_ratio();
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////
- ////////////////////                  Modifier                            ////////////////////////////// 
+ ////////////////////                  Modifier                           
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+  modifier  moreThenZero(uint _amount) {
+    if(_amount == 0 ){
+      revert XP_amount_zero() ;
+    }
+    _;    
+  }
+
   ////////////////////////////////////////////////////////////////////////////////////////////////////////
- ////////////////////                   Events                             ////////////////////////////// 
+ ////////////////////                   Events                            
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////
- ////////////////////                  Function                            ////////////////////////////// 
+ ////////////////////                  Function                            
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   function deposit_colletral_mintXP() external {}
 
   function redeem_colletral_for_XP()external {}
 
-  function mint_XP(uint _amount)external  onlyOwner returns(bool ){
-    if(_amount <=0){
-      revert XP_amount_zero();
-    }
-    else {
-      Stablecoin.mint();
-    }
+  function mint_XP(uint _amount)external moreThenZero(_amount) returns(bool ){
+  
+      x_XPMinted[msg.sender] += _amount ;
+
+     bool status =  stablecoin.mint(msg.sender , _amount );
+     return status ;
+  
    
   }
 
