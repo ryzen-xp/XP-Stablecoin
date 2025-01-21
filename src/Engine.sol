a80fe7e336f543a493f0593a7d25a207c112e0e2
Ryzen XP <iron4540@gmail.com>
2025-01-21 19:46:23 +0530

    add some new thigs

--- src/Engine.sol
+++ src/Engine.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+
+/**
+ * @title this is Stablecoin Engine  , its manages all functionality .
+ * @author ryzen_xp 
+ * @notice 
+ */
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {Stablecoin} from "./stablecoin.sol";
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-
 import "../lib/chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract XP_Engine {
     AggregatorV3Interface internal eth_usd_PriceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+
 
     constructor(address[] memory CollateralAddress, address[] memory price_feed_address, address xp_address) {
         if (CollateralAddress.length != price_feed_address.length) {
@@ -41,7 +48,7 @@ contract XP_Engine {
     mapping(address colletralAddress => address price_feed_address) private mp_price_feed;
 
     // User minted XP token mapping( user => amount_of_XPTokens ) : :
-    mapping(address => uint256) private xp_XPMinted;
+    mapping(address => uint256) private xp_minted;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////                  ERRORS
@@ -67,7 +74,7 @@ contract XP_Engine {
             revert XP_notAllowed_Token();
         }
         _;
-    }
+    } 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////                   Events
@@ -78,6 +85,7 @@ contract XP_Engine {
     ////////////////////                  Function
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// this deposit_colletral_mintXp is used to collect assest which user want to deposite in contract
     function deposit_colletral_mintXP(address collatreal_address, uint256 _amount, uint256 amountToXP_mint) external {
         deposite_collatreal(collatreal_address, _amount);
         mint_XP(amountToXP_mint);
@@ -85,15 +93,19 @@ contract XP_Engine {
 
     function redeem_colletral_for_XP() external {}
 
+
+// this mint_XP is use to mint the ERC20 token 
     function mint_XP(uint256 _amount) private moreThenZero(_amount) returns (bool) {
-        xp_XPMinted[msg.sender] += _amount;
+        xp_minted[msg.sender] += _amount;
 
         bool status = i_XP.mint(msg.sender, _amount);
         return status;
     }
 
+
+// this burn_XP is use to burn minted token ERC20 from blochain 
     function burn_XP(address behalf ,address _to ,  uint _amount) private moreThenZero(_amount){
-      xp_XPMinted[behalf] -= _amount ;
+      xp_minted[behalf] -= _amount ;
 
       bool success = i_XP.transferFrom(_to , address(this) , _amount);
       if(!success){
@@ -104,7 +116,13 @@ contract XP_Engine {
 
     }
 
-    function liquidate() external {}
+
+
+// liquidation function is here  this
+
+    function liquidate(address _collatreal_address , address user  , uint _amount ) private  {
+        
+    }
    
   //  deposite_collatreal takes addres of asset and amount  to deposite  in this contract 
     function deposite_collatreal(address collatreal_address, uint256 _amount)
@@ -113,12 +131,18 @@ contract XP_Engine {
         isAllowed_Token(collatreal_address)
         returns (bool)
     {
-        mp_colletralDeposite[msg.sender][collatreal_address] = _amount;
+        mp_colletralDeposite[msg.sender][collatreal_address] += _amount;
 
         emit XP_deposited_collatreal(msg.sender, collatreal_address, _amount);
 
         bool success = IERC20(collatreal_address).transferFrom(address(msg.sender), address(this), _amount);
 
         return success;
+    }
+
+    // Helthfactor 
+
+    function helthfactor() private {
+
     }
 }
